3f2e966acb26d95b7a1bc206a66e92b284bac014
Grant Glidewell <ggcadc@gmail.com>
2017-08-15 13:12:41 -0700

    added some better looks

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const nav = document.querySelector("#main");
+const about = document.querySelector("#about");
+const project = document.querySelector("#projects");
+const contact = document.querySelector("#contact");
 const topOfNav = nav.offsetTop;
 
 function fixNav() {
+  if (window.scrollY === 0) {
+    clearActive();
+  }
   if (window.scrollY >= topOfNav) {
     document.body.style.paddingTop = nav.offsetHeight + "px";
     document.body.classList.add("fixed-nav");
@@ -10,5 +16,19 @@ function fixNav() {
     document.body.classList.remove("fixed-nav");
   }
 }
+
+const highlightActive = (item) => {
+  about.style = 'color: white';
+  project.style = 'color: white';
+  contact.style = 'color: white';
+  return item.style = 'color: rgb(150, 4, 4)';
+}
+
+const clearActive = () => {
+  about.style = 'color: white';
+  project.style = 'color: white';
+  contact.style = 'color: white';
+}
+
 
 window.addEventListener("scroll", fixNav);
